d3e5e576d28fe430a09d0447fb8a04f20406df2e
David Thomann <david.joshua.thomann@gmail.com>
2025-11-01 16:59:43 +0100

    Removed Generated Platform components and added a NavBar

--- composeApp/src/commonMain/kotlin/hsrm/mi/campusapp/App.kt
+++ composeApp/src/commonMain/kotlin/hsrm/mi/campusapp/App.kt
@@ -3,12 +3,26 @@ package hsrm.mi.campusapp
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material.icons.filled.CalendarToday
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Map
+import androidx.compose.material.icons.outlined.CalendarMonth
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Map
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -22,27 +36,53 @@ import campusapp.composeapp.generated.resources.compose_multiplatform
 @Composable
 @Preview
 fun App() {
+
+
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+        Scaffold(
+            bottomBar = {
+                NavigationBar {
+
+                    var selectedItem by remember { mutableIntStateOf(1) }
+
+                    NavigationBarItem(
+                        selected = selectedItem == 0,
+                        onClick = { selectedItem = 0 },
+                        icon = {
+                            Icon(
+                                imageVector = if (selectedItem == 0) Icons.Filled.CalendarMonth else Icons.Outlined.CalendarMonth,
+                                contentDescription = "Calendar"
+                            )
+                        },
+
+                    )
+                    NavigationBarItem(
+                        selected = selectedItem == 1,
+                        onClick = { selectedItem = 1 },
+                        icon = {
+                            Icon(
+                                imageVector = if (selectedItem == 1) Icons.Filled.Home else Icons.Outlined.Home,
+                                contentDescription = "Home"
+                            )
+                        },
+
+                    )
+                    NavigationBarItem(
+                        selected = selectedItem == 2,
+                        onClick = { selectedItem = 2 },
+                        icon = {
+                            Icon(
+                                imageVector = if (selectedItem == 2) Icons.Filled.Map else Icons.Outlined.Map,
+                                contentDescription = "Map"
+                            )
+                        },
+
+                        )
                 }
+            }
+        ) { innerPadding ->
+            Box(Modifier.padding(innerPadding)) {
+
             }
         }
     }
--- composeApp/src/commonMain/kotlin/hsrm/mi/campusapp/Platform.kt
+++
@@ -1,7 +0,0 @@
-package hsrm.mi.campusapp
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
