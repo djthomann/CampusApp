04d7d59e614f177bb24e3fc8540a2b9f6c03a0e8
David Thomann <david.joshua.thomann@gmail.com>
2025-11-05 20:31:58 +0100

    Made Android Map Screen the same as Desktop Screen

--- composeApp/src/androidMain/kotlin/hsrm/mi/campusapp/screen/MapScreen.android.kt
+++ composeApp/src/androidMain/kotlin/hsrm/mi/campusapp/screen/MapScreen.android.kt
@@ -1,9 +1,48 @@
 package hsrm.mi.campusapp.screen
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import io.github.dellisd.spatialk.geojson.Position
+import org.maplibre.compose.camera.CameraPosition
+import org.maplibre.compose.camera.rememberCameraState
+import org.maplibre.compose.map.GestureOptions
+import org.maplibre.compose.map.MapOptions
 import org.maplibre.compose.map.MaplibreMap
+import org.maplibre.compose.map.OrnamentOptions
+import org.maplibre.compose.map.RenderOptions
+import org.maplibre.compose.style.BaseStyle
+import org.maplibre.compose.style.rememberStyleState
 
 @Composable
 actual fun MapView() {
-    MaplibreMap()
+    val cameraState = rememberCameraState(
+        firstPosition = CameraPosition(
+            target = Position( 8.217, 50.0964),
+            zoom = 16.0,
+            tilt = 45.0,
+            bearing = 0.0
+        )
+    )
+
+    val styleState = rememberStyleState()
+
+    val mapOptions = MapOptions(
+        renderOptions = RenderOptions.Standard,
+        gestureOptions = GestureOptions.Standard,
+        ornamentOptions = OrnamentOptions(
+            isCompassEnabled = true,
+            isLogoEnabled = true
+        )
+    )
+
+    MaplibreMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraState = cameraState,
+        styleState = styleState,
+        options = mapOptions,
+        baseStyle = BaseStyle.Uri("https://tiles.openfreemap.org/styles/liberty"),
+    ) {
+
+    }
 }
