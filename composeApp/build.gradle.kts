83ddedca9094ef96e7b8436ed435ccf3f6473390
David Thomann <david.joshua.thomann@gmail.com>
2026-02-09 17:04:21 +0100

    Added dependencies for Ktor Client testing

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -73,6 +73,7 @@ kotlin {
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+            implementation(libs.ktor.client.mock)
         }
         jvmMain.dependencies {
             implementation(compose.desktop.currentOs)
